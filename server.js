a6d5f597e6b4bf477e32fff66da8a8cd26ca409f
Nikhilamathi <30805455+Nikhilamathi@users.noreply.github.com>
2017-08-27 11:38:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+var content={
+ title:'Article one Nikhilamathi !'   
+};
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
