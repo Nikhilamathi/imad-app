b31b2d53be2da3664dec0c92ddc3394ae917fa1d
Nikhilamathi <30805455+Nikhilamathi@users.noreply.github.com>
2017-08-28 23:37:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
-var articleOne={
+var articles={ 
+    'article-one':{
  title:'Article one Nikhilamathi !',   
  heading:'Article one',
  date:'sep 6,2017',
@@ -22,6 +23,30 @@ var articleOne={
                     Cannot see the ripped up emotions
                     Of reality.
                 </p>`
+},
+'article-two':{ title:'Article two Nikhilamathi !',
+heading:'Article two',
+date:' Aug 26,2017',
+content:`<p>
+            This is the first content of Article two
+                </p>
+                <p>
+                Photoshop is my passion.I love to edit pictures of other people.Whenever i do photoshop i forgot the sorrows in life and be happy
+                </p>`
+    
+},
+'article-three':{
+    title:'Article three Nikhilamathi !',
+heading:'Article three',
+date:' Aug 27 ,2017',
+content:`<p>
+            This is the first content of Article three
+                </p>
+                <p>
+                Learning and doing something on our own gives total happiness to me.Happy to create this site now.
+                </p>`
+    
+}
 };
 function createTemplate (data) {
     var title=data.title;
@@ -63,14 +88,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function (req, res) {
-res.send(createTemplate(articleOne));
-});
-app.get('/article-two', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function (req, res) {
-res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+app.get('/:articleName', function (req, res) {
+ var articleName=req.params.articleName 
+res.send(createTemplate(articles[articleName]));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
