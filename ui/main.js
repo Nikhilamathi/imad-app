39a5e117b8ebb13242a025ef01d775bff06d208f
Nikhilamathi <30805455+Nikhilamathi@users.noreply.github.com>
2017-08-31 00:24:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //
 var element = document.getElementById('Main-txt');
 element.innerHTML= 'Nikhila'
+// move the image when we click 
+var img=document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft= '100px';
+};
