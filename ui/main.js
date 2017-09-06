fa881e28f00e6bb191c874b3a93c9921d9a21f74
Nikhilamathi <30805455+Nikhilamathi@users.noreply.github.com>
2017-09-06 22:40:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,23 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 //
-var element = document.getElementById('Main-txt');
-element.innerHTML= 'Nikhila'
+//var element = document.getElementById('Main-txt');
+//element.innerHTML= 'Nikhila'
 // move the image when we click 
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft= marginLeft + 5;
-    img.style.marginLeft=marginLeft + 'px';
-}
-img.onclick = function(){
-    var interval=setInterval(moveRight,50);
+//var img=document.getElementById('madi');
+//var marginLeft=0;
+//function moveRight(){
+   // marginLeft= marginLeft + 5;
+    //img.style.marginLeft=marginLeft + 'px';
+//}
+//img.onclick = function(){
+   // var interval=setInterval(moveRight,50);
     //img.style.marginLeft= '100px';
+//}
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function (){
+    counter = counter+1;
+    var span = document.getElementById('counts');
+    span.innerHTML =counter.toString();
 }
+
