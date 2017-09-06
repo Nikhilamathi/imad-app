cb370296a472ad41f30ae9f2eb133d87bd4a3670
Nikhilamathi <30805455+Nikhilamathi@users.noreply.github.com>
2017-09-06 23:00:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,15 +14,16 @@
     //img.style.marginLeft= '100px';
 //}
 button.onclick = function (){
+    
 var button = document.getElementById('counter');
 var counter=0;
 //make request to counter endpoint 
-var request = new XMLHttprequest();
+var request = new XMLHttpRequest();
 
 //Capture the response and store it in a variable
 request.onreadystatechange=function()
 {
-  if ( request.readyState==XMLHttprequest.DONE)
+  if ( request.readyState==XMLHttpRequest.DONE)
   {
       //take some action
       if (request.status==200)
